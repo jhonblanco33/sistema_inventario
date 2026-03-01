d80547529b0ef4d015518c8d99678ffbf0a2284a
jhon <Jhonblanco970@gmail.com>
2026-03-01 18:08:32 -0500

    Refactor de la primera funcion

--- inventario.py
+++ inventario.py
@@ -1,14 +1,20 @@
 # test_almacen.py
+from typing import List, Dict, Union
+
 class Almacen:
     def __init__(self):
-        self.articulos = []
+        
+        self.articulos: List[Dict[str, Union[str, int, float]]] = []
 
-    def agregar_item(self, descripcion, stock, costo):
+    def agregar_item(self, descripcion: str, stock: int, costo: float) -> bool:
+       
+        if stock < 0 or costo < 0:
+            return False
+            
         item = {
             "descripcion": descripcion,
             "stock": stock,
             "costo": costo
-            
         }
         self.articulos.append(item)
         return True
--- test_inventario.py
+++ test_inventario.py
@@ -2,12 +2,12 @@
 
 from inventario import Almacen
 
-
 def test_agregar_item():
     mi_almacen = Almacen()
-    resultado = mi_almacen.agregar_item("Monitor 24 pulgadas", 12, 450.0)
+    
 
-    assert resultado == True
-    assert mi_almacen.articulos[0]["descripcion"] == "Monitor 24 pulgadas"
-    assert mi_almacen.articulos[0]["stock"] == 12
-    assert mi_almacen.articulos[0]["costo"] == 450.0
+    assert mi_almacen.agregar_item("Monitor 24 pulgadas", 12, 450.0) == True
+    assert len(mi_almacen.articulos) == 1
+    
+
+    assert mi_almacen.agregar_item("Teclado Dañado", -5, 20.0) == False
