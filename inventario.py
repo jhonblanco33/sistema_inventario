695ebbf8c1ffe4aa7e949d921d1a91c38b3783fb
jhon <Jhonblanco970@gmail.com>
2026-03-01 18:13:33 -0500

    Creacion y refactorizacion de la segunda funcion de modificar stock

--- inventario.py
+++ inventario.py
@@ -3,19 +3,26 @@ from typing import List, Dict, Union
 
 class Almacen:
     def __init__(self):
-        
         self.articulos: List[Dict[str, Union[str, int, float]]] = []
 
     def agregar_item(self, descripcion: str, stock: int, costo: float) -> bool:
-       
         if stock < 0 or costo < 0:
             return False
-            
-        item = {
-            "descripcion": descripcion,
-            "stock": stock,
-            "costo": costo
-        }
+        item = {"descripcion": descripcion, "stock": stock, "costo": costo}
         self.articulos.append(item)
         return True
+
+    def modificar_stock(self, descripcion: str, nuevo_stock: int) -> bool:
+        # Validación: No permitir stock negativo
+        if nuevo_stock < 0:
+            return False
+            
+        # Buscar el artículo real y actualizarlo
+        for item in self.articulos:
+            if item["descripcion"] == descripcion:
+                item["stock"] = nuevo_stock
+                return True
+                
+    
+        return False
 
